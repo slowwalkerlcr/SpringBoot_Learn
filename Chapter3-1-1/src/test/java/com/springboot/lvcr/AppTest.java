0e81f09113f10b88e495f7b456f70dd7f5508c29
lvchangrong <slowwalkerlcr@139.com>
2017-04-02 17:04:18 +0800

    构建一个较为复杂的RESTful API以及单元测试

--- Chapter3-1-1/src/test/java/com/springboot/lvcr/AppTest.java
+++ Chapter3-1-1/src/test/java/com/springboot/lvcr/AppTest.java
@@ -1,38 +1,94 @@
 package com.springboot.lvcr;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+import com.springboot.lvcr.controller.HelloController;
+import com.springboot.lvcr.controller.UserController;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = MockServletContext.class)
+// @SpringApplicationConfiguration(classes = MockServletContext.class)
+@WebAppConfiguration
+public class AppTest {
+
+	private MockMvc mvc;
+
+	@Before
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(new HelloController(),
+				new UserController()).build();
+	}
+
+	@Test
+	public void getHello() throws Exception {
+		mvc.perform(
+				MockMvcRequestBuilders.get("/hello").accept(
+						MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Hello World")));
+		
+
+	}
+
+	@Test
+	public void testUserController() throws Exception {
+		// 测试UserController
+		RequestBuilder request = null;
+
+		// 1、get查一下user列表，应该为空
+		request = get("/users/");
+		mvc.perform(request).andExpect(status().isOk())
+				.andExpect(content().string(equalTo("[]")));
+
+		// 2、post提交一个user
+		request = post("/users/").param("id", "1").param("name", "测试大师")
+				.param("age", "20");
+		mvc.perform(request)
+		// .andDo(MockMvcResultHandlers.print())
+				.andExpect(content().string(equalTo("success")));
+
+		// 3、get获取user列表，应该有刚才插入的数据
+		request = get("/users/");
+		mvc.perform(request)
+				.andExpect(status().isOk())
+				.andExpect(
+						content()
+								.string(equalTo("[{\"id\":1,\"name\":\"测试大师\",\"age\":20}]")));
+
+		// 4、put修改id为1的user
+		request = put("/users/1").param("name", "测试终极大师").param("age", "30");
+		mvc.perform(request).andExpect(content().string(equalTo("success")));
+
+		// 5、get一个id为1的user
+		request = get("/users/1");
+		mvc.perform(request).andExpect(
+				content().string(
+						equalTo("{\"id\":1,\"name\":\"测试终极大师\",\"age\":30}")));
+
+		// 6、del删除id为1的user
+		request = delete("/users/1");
+		mvc.perform(request).andExpect(content().string(equalTo("success")));
+
+		// 7、get查一下user列表，应该为空
+		request = get("/users/");
+		mvc.perform(request).andExpect(status().isOk())
+				.andExpect(content().string(equalTo("[]")));
+
+	}
+
 }
