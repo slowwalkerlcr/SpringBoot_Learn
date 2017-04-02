99adbcc2b3c3068263c2085735fd3963c974896e
lvchangrong <slowwalkerlcr@139.com>
2017-04-02 15:27:39 +0800

    基本项目构建（可作为工程脚手架），引入web模块，完成一个简单的RESTful API

---
+++ Chapter1/src/main/java/com/springboot/lvcr/web/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.springboot.lvcr.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	@RequestMapping("/hello")
+	public String index() {
+		return "hello world";
+	}
+
+}
