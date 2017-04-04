cc72737f59ddd946f3867988bed804e07c0224af
EdisonLv <slowwalkerlcr@163.com>
2017-04-04 22:24:07 +0800

    统一异常处理

---
+++ Chapter3-1-6/src/main/java/com/springboot/lvcr/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.springboot.lvcr.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.springboot.lvcr.dto.ErrorInfo;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	@ExceptionHandler(value = MyException.class)
+	    @ResponseBody
+	    public ErrorInfo<String> jsonErrorHandler(HttpServletRequest req, MyException e) throws Exception {
+	        ErrorInfo<String> r = new ErrorInfo<>();
+	        r.setMessage(e.getMessage());
+	        r.setCode(ErrorInfo.ERROR);
+	        r.setData("Some Data");
+	        r.setUrl(req.getRequestURL().toString());
+	        return r;
+	    }
+}
---
+++ Chapter3-1-6/src/main/java/com/springboot/lvcr/exception/MyException.java
@@ -0,0 +1,13 @@
+package com.springboot.lvcr.exception;
+
+public class MyException extends Exception {
+
+	/**  
+	 * serialVersionUID:TODO(用一句话描述这个变量表示什么).  
+	 */
+	private static final long serialVersionUID = 7746108145588185510L;
+
+	public MyException(String message) {
+		super(message);
+	}
+}
